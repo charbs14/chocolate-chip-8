52f280415bdd90981fd984e324ae37099e396ab2
Lucas Reeves <lreeves0000@gmail.com>
2024-07-26 13:13:21 -0700

    wrote load_rom function

--- chip8.c
+++ chip8.c
@@ -4,6 +4,7 @@
 #include "chip8.h"
 #include <iostream>
 #include <stdint>
+#include <stdio>
 
 	uint8_t mem[4096] = {//4kb of mem, initialized with fonts 
 		
@@ -26,22 +27,65 @@
 	
 	};
 
-	uint8_t display[64 * 32] = {0}; //64 * 32 pixel display
+	uint8_t display[64 * 32];  //64 * 32 pixel display
+					
 					
 	uint16_t pc = 0x200; //program counter, begins at 0x200 in mem since 
 			     //interpreters were usually stored before that address
 	
 	uint16_t i = 0; //index register used to point at locations in memory
 
-	uint16_t stack[128] = {0}; //stack for subroutines and functions
+	uint16_t stack[128]; //stack for subroutines and functions
 				   
 	uint8_t delay = 60; //delay timer, decremented 60 times per sec until it reaches 0
 			    
 	uint8_t sound = 0; //sound timer which gives off beeping sound as long as its not 0
 	
-	uint8_t V[16] = {0}; //16 one byte general purpose registers. 
+	uint8_t V[16]; //16 one byte general purpose registers. 
 			     //V[15] or VF is used as a flag register
+
+	void load_rom(char* file){
+		FILE* rom;
+
+		rom = fopen(file, "rb");//open file to read binary
+		if(rom == NULL){
+			printf("Failed to open ROM\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		fseek(rom, 0, SEEK_END);
+		int rom_size = ftell(rom);
+
+		if(rom_size + 0x200 > 4096){
+			printf("Rom exceedes 4096kb mem size");
+			exit(EXIT_FAILURE);
+		}
+		
+		rewind(rom);
+		fread(&mem[0x200],sizeof(uint16_t),rom_size, rom);
+		fclose(rom);
+
+		return;
+
+	}
 	
 
-	
+	int main(int argc, char *argv[]){
+			
+		bool exit = 0;
+		
+		if(argc != 2){
+			printf("Usage: %s <rom file>", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		
+		load_rom(argv[1]);
+
+
+		while(!exit){
+
+		}
+
+
+	}
 
